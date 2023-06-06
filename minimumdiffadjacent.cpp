6bef531e0b5172fbbed5f692c2b1904a700f3315
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-06 13:13:07 +0530

    blah blah blah

---
+++ minimumdiffadjacent.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to find minimum adjacent difference in a circular array.
+    //  arr[]: input array
+    //  n: size of array
+    int minAdjDiff(int arr[], int n)
+    {
+        // Your code here
+        int min_diff = INT_MAX;
+        for (int i = 0; i < n - 1; i++)
+        {
+            int diff = arr[i] - arr[i + 1];
+            min_diff = min(min_diff, diff);
+        }
+        return min_diff;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin >> t; // Input testcases
+
+    while (t--) // While testcases exist
+    {
+        int n;
+        cin >> n; // Input size of array
+
+        int arr[n]; // Array of size n
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i]; // input elements of array
+        }
+        Solution ob;
+
+        cout << ob.minAdjDiff(arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
