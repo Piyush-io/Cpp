55fbf7c8815fbe5d75be3f8d7a64380ab76ec6c5
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-13 21:15:09 +0530

    not correct but working on it

--- subarraywithgivensum.cpp
+++ subarraywithgivensum.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int subsum(int *arr , int n , int k)
+int subsum(int *arr, int n, int k)
 {
-    
+    int sum = 0;
+    int res = INT_MIN;
+    for(int i = 0 ; i < n ; i++)
+    {
+        sum += arr[i];
+        for(int j = i+1 ; j < n ; j++)
+        {
+            sum += arr[j];
+            res = max(res,sum);
+        }
+    }
+    return res;
 }
 
 int main()
@@ -14,7 +25,8 @@ int main()
     int *arr = new int[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i] ;
+        cin >> arr[i];
     }
-
+    int sum = subsum(arr, n, k);
+    cout << sum << endl;
 }
