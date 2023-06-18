8ee95d5b526dc460aab8b2ce3ee1e8f0b9b86d3c
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-18 12:28:11 +0530

    Pushing

--- numberofoccurance.cpp
+++ numberofoccurance.cpp
@@ -1,62 +1,79 @@
 #include <iostream>
 using namespace std;
 
-int firstocc(int *arr, int i, int l, int k, int n)
+int firstOcc(int arr[], int n, int x)
 {
-    while (i <= l)
-    {
-        int mid = (i + l) / 2;
+	int low = 0, high = n - 1;
 
-        if (mid == 0 && arr[mid] == k || (arr[mid] == k && arr[mid] != arr[mid - 1]))
-        {
-            return mid;
-            break;
-        }
+	while(low <= high)
+	{
+		int mid = (low + high) / 2;
 
-        else if (arr[mid] > k)
-        {
-            l = mid - 1;
-        }
-        else
-        {
-            i = mid + 1;
-        }
-    }
-    return -1;
+		if(x > arr[mid])
+			low = mid + 1;
+
+		else if(x < arr[mid])
+			high = mid - 1;
+
+		else
+		{
+			if(mid == 0 || arr[mid - 1] != arr[mid])
+				return mid;
+
+			else
+				high = mid - 1;
+		}
+
+	}
+
+	return -1;
 }
 
-int lastocc(int *arr, int i, int l, int k, int n)
+int lastOcc(int arr[], int n, int x)
 {
-    while (i <= l)
-    {
-        int mid = (i + l) / 2;
+	int low = 0, high = n - 1;
 
-        if ((mid == n - 1 && arr[mid] == k) || (arr[mid] == k && arr[mid] != arr[mid] + 1))
-        {
-            return mid;
-            break;
-        }
+	while(low <= high)
+	{
+		int mid = (low + high) / 2;
 
-        else if (arr[mid] > k)
-        {
-            l = mid - 1;
-        }
-        else
-        {
-            i = mid + 1;
-        }
-    }
-    return -1;
+		if(x > arr[mid])
+			low = mid + 1;
+
+		else if(x < arr[mid])
+			high = mid - 1;
+
+		else
+		{
+			if(mid == n - 1 || arr[mid + 1] != arr[mid])
+				return mid;
+
+			else
+				low = mid + 1;
+		}
+
+	}
+
+	return -1;
 }
 
-int main()
+int countOcc(int arr[], int n, int x)
 {
-    int arr[] = {1, 1, 2, 2, 2, 2, 3};
-    int n = 7;
-    int k;
-    cin >> k;
-    int i = firstocc(arr, 0, n - 1, k, n);
-    int j = lastocc(arr, 0, n - 1, k, n);
-    int count = j - i + 1;
-    cout << count;
+	int first = firstOcc(arr, n, x);
+
+	if(first == -1)
+		return 0;
+	else 
+		return lastOcc(arr, n, x) - first + 1;
+}
+
+int main() {
+    
+   int arr[] = {10, 20, 20, 20, 40, 40}, n = 6;
+
+   int x = 20;
+
+   cout << countOcc(arr, n, x);
+
+	return 0;
 }
