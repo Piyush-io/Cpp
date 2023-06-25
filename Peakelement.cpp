895b62bf3d77050c95757567e39739da6d143d0c
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-25 19:25:26 +0530

    bruhhhh

---
+++ Peakelement.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to find the minimum element in sorted and rotated array.
+    int peak(int arr[], int low, int high)
+    {
+        while (low < high)
+        {
+            
+        }
+        
+    }
+};
+
+int main()
+{
+     int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
+        Solution obj;
+        cout << obj.peak(a, 0, n - 1) << endl;
+}
