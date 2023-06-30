9ba71d93f2c0b53460a76a67d81e75a44e1846b3
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-30 08:10:26 +0530

    getting better

--- repeatingelements.cpp
+++ repeatingelements.cpp
@@ -5,11 +5,36 @@ using namespace std;
 // all elements present from 0 to arr[max]
 // every element appears once except one
 
-int repeatingelement(int *arr , int n)
-{
+int repeatingelement(int *arr, int n)
+{   
 
+    bool *A = new bool[n-1];
+    memset(A,false,n-1);
+    for(int i = 0 ; i < n ; i++)
+    {   
+        if (A[arr[i]])
+        {
+            return arr[i];
+        }
+        else
+            A[arr[i]] = true;
+    }
+    delete[] A;
+    return -1;
 }
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    // int arr[n];
+    const int mx = 1e9;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int res = repeatingelement(arr, n);
+    cout << res << endl;
+    delete[] arr;
 }
