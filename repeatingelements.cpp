abc39e9c38b868d3d9bc6c5bb70fea52defc7091
Piyush Mittal <mittalpiyush2004@gmail.com>
2023-06-30 08:10:10 +0530

    getting better

---
+++ repeatingelements.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// 0 is included
+// all elements present from 0 to arr[max]
+// every element appears once except one
+
+int repeatingelement(int *arr , int n)
+{
+
+}
+int main()
+{
+    
+}
