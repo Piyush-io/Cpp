c8630250f4a9cabfef4c4d163467af8be05b4061
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-08 21:53:56 +0530

    done

--- squareroot.cpp
+++ squareroot.cpp
@@ -3,28 +3,8 @@ using namespace std;
 
 int floorSqrt(int x)
 { // Base cases
-    if (x == 0 || x == 1)
-        return x;
-
-    int start = 1, end = x / 2, ans;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-
-        // If x is a perfect square
-        int sqr = mid * mid;
-        if (sqr == x)
-            return mid;
-
-        if (sqr <= x)
-        {
-            start = mid + 1;
-            ans = mid;
-        }
-        else // If mid*mid is greater than x
-            end = mid - 1;
-    }
-    return ans;
+    int res = sqrt(x);
+    return (int)res;
 }
 
 int main()
