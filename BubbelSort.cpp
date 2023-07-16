8044b0b2182c4941ab3401cf6dd46de8f938b662
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-16 16:57:04 +0530

    better redability

--- BubbelSort.cpp
+++ BubbelSort.cpp
@@ -6,9 +6,9 @@ int main()
     int arr[] = {5, 18, 2, 3, 6, 3};
     int n = 6;
 
-    for (int i = n - 1; i > 0; i--)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
