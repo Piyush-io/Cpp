446a961905c40e5101ae071a0feaa3530d1b57ca
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-18 07:43:52 +0530

    done

--- mergefunctionofmergesort.cpp
+++ mergefunctionofmergesort.cpp
@@ -2,29 +2,27 @@
 using namespace std;
 
 void mergearray(int *arr, int low, int mid, int high)
-{   
+{
     // we start by creating a set of arrays
-    int N = mid - low + 1;  //first array of size low to mid (first sorted array)
-    int n = high - mid;  //second array of size mid+1 to high (second sorted array)
+    int N = mid - low + 1; // first array of size low to mid (first sorted array)
+    int n = high - mid;    // second array of size mid+1 to high (second sorted array)
     int a1[N];
     int a2[n];
-    int k = low;
     // copying the elements of the first sorted part of the original array to the first created array
     for (int i = 0; i < N; i++)
     {
-        a1[i] = arr[k++];
+        a1[i] = arr[low + i];
     }
     // copying the second sorted part to the second created array
     int z = mid + 1;
     for (int i = 0; i < n; i++)
     {
-        a2[i] = arr[z++];
+        a2[i] = arr[mid + 1 + i];
     }
-
     // then we simply use merge two sorted arrays logic to modify the original array according to the order
     int i = 0;
     int j = 0;
-    k = low;
+    int k = low;
     while (i < N && j < n)
     {
         if (a1[i] <= a2[j])
@@ -53,10 +51,10 @@ void mergearray(int *arr, int low, int mid, int high)
 
 int main()
 {
-    int arr[] = {10, 15, 20, 5, 6, 6, 15};
-    mergearray(arr, 0, 2, 6);
+    int arr[] = {1, 4, 2, 3, 10, 15, 20, 5, 6, 6, 15, 10, 3, 4};
+    mergearray(arr, 4, 6, 11);
 
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 14; i++)
     {
         cout << arr[i] << " ";
     }
