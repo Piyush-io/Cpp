450da1e05d526d1792bbb095d86e7132f5c11021
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-19 10:32:47 +0530

    bruhhh i'm done

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,25 @@
-res = arr[i];
-                count++;
-            
+if (n < 3)
+            return 0;
+        sort(arr, arr + n);
+        int i = 0;
+        int j = n - 1;
+        int count = j - i + 1;
+        while (i < j && count >= 3)
+        {
+            int sum = 0;
+            for (int z = i; z <= j; z++)
+            {
+                sum += arr[z];
+            }
+            cout << sum << endl;
+            if (sum == 0 && count == 3)
+                return true;
+            else if (sum < 0)
+            {
+                i++;
+            }
+            else
+                j--;
+            count = j - i + 1;
+        }
+        return false;
