a768513eac469bdc7bdc9debba2cadc014bc88a9
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-24 11:30:50 +0530

    i'm too old for this crud

---
+++ chocolatedistribution.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void distribute(int *arr, int n)
+{
+    sort(arr , arr+n);
+    int diff
+}
+
+int main()
+{
+    int arr[] = {3, 4, 1, 9, 56, 7, 9, 12};
+    distribute(arr, 8);
+}
