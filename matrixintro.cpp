d05b00e091809a926cd6caac642f12687b3a1d5e
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-07-30 12:53:48 +0530

    bruh this is rubbish

--- matrixintro.cpp
+++ matrixintro.cpp
@@ -1,74 +1,83 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 #include <iostream>
-using namespace std; 
+using namespace std;
 
 // } Driver Code Ends
 
 class Solution
-{   
-    public:
-    //Function to add two matrices.
-    vector<vector<int> > sumMatrix( const vector<vector<int> >& vect1, const vector<vector<int> >& vect2)
+{
+public:
+    // Function to multiply two matrices.
+    vector<vector<int>> multiplyMatrix(const vector<vector<int>> &A, const vector<vector<int>> &B)
     {
-    
-    vector<vector <int>> v;    
-        
-    for (int i = 0; i < vect1.size(); i++)
-    {
-        vector <int> temp;
-        for (int j = 0; j < vect1[i].size(); j++)
+        // code here
+        vector<vector<int>> res(A.size(), vector<int>(B[0].size(), 0));
+        if (A.size() != B[0].size())
         {
-            temp.push_back(vect1[i][j] + vect2[i][j]);
+            return res;
         }
-        v.push_back(temp);
-    }
-    return v;
+
+        for (int i = 0; i < A[0].size(); i++)
+        {   
+            for (int j = 0; j < B.size(); j++)
+            {
+                for (int k = 0; k < A[0].size() ; k++)
+                {
+                    res[i][k] += A[i][k]*B[j][k];
+                }
+            }
+        }
+        return res;
     }
 };
 
 //{ Driver Code Starts.
-int main() {
+
+int main()
+{
     int t;
-    cin>>t;
-    while(t--) 
+    cin >> t;
+    while (t--)
     {
         int row, col;
-        cin>> row>> col;
-        vector<vector<int> > A(row); 
-        for(int i=0; i<row; i++)
+        cin >> row >> col;
+        vector<vector<int>> A(row);
+        for (int i = 0; i < row; i++)
         {
             A[i].assign(col, 0);
-            for( int j=0; j<col; j++)
+            for (int j = 0; j < col; j++)
             {
-                cin>>A[i][j];
+                cin >> A[i][j];
             }
         }
-        cin>> row>> col;
-        vector<vector<int> > B(row);
-        for(int i=0; i<row; i++)
+        cin >> row >> col;
+        vector<vector<int>> B(row);
+        for (int i = 0; i < row; i++)
         {
             B[i].assign(col, 0);
-            for( int j=0; j<col; j++)
+            for (int j = 0; j < col; j++)
             {
-                cin>>B[i][j];
+                cin >> B[i][j];
             }
         }
         Solution ob;
-        vector<vector<int> > result = ob.sumMatrix(A,B);
-        if(result.size() == 0)
-            cout<<"-1";
+        vector<vector<int>> result = ob.multiplyMatrix(A, B);
+
+        if (result.size() == 0)
+            cout << "-1";
         else
         {
             for (int i = 0; i < result.size(); ++i)
             {
                 for (int j = 0; j < result[0].size(); ++j)
                 {
-                    cout<<result[i][j]<<" ";
+                    cout << result[i][j] << " ";
                 }
+                cout << endl;
             }
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
