eaa110ef49de48e8520d333ea61f85086f8b8ec6
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-01 07:38:57 +0530

    i'm done

--- beautifulmatrix.cpp
+++ beautifulmatrix.cpp
@@ -1,78 +1,82 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
 
 // } Driver Code Ends
+
+
 class Solution
 {
-public:
-    // Function to find minimum number of operations that are required
-    // to make the matrix beautiful.
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
     int findMinOpeartion(vector<vector<int> > matrix, int n)
     {
-        // code here
-        int max_sum = 0;
-        for (int i = 0; i < n; i++)
-        {
+        // code here 
+        int targetsum = INT_MIN;
+        for(int i = 0 ; i < n ; i++)
+        {   
             int sum = 0;
-            for (int j = 0; j < n; j++)
+            for(int j = 0 ;  j < n ; j++)
             {
                 sum += matrix[i][j];
             }
-            max_sum = max(max_sum, sum);
+            targetsum = max(targetsum,sum);
         }
-        for (int i = 0; i < n; i++)
-        {
+        for(int i = 0 ; i < n ; i++)
+        {   
             int sum = 0;
-            for (int j = 0; j < n; j++)
+            for(int j = 0 ;  j < n ; j++)
             {
                 sum += matrix[j][i];
             }
-            max_sum = max(max_sum, sum);
+            targetsum = max(targetsum,sum);
         }
-
+        
         int operations = 0;
-
-        for (int i = 0; i < n; i++)
+        
+        for(int i = 0; i < n ; i++)
         {
-            int sum = 0;
-            for (int j = 0; j < n; j++)
+            int curr_row_sum = 0;
+            for(int j = 0 ; j < n ; j++)
             {
-                sum += matrix[i][j];
+                curr_row_sum += matrix[i][j];
             }
-            if (sum < max_sum)
-            {
-                int temp_sum = 0;
-                for (int j = 0; j < n; j++)
-                {
-                    temp_sum += matrix[i][j];
-                }
-            }
-            
+            int diff = maxsum - curr_row_sum;
+            operations += diff;
         }
-
-        return operations;
-    }
+        
+        for(int i = 0; i < n ; i++)
+        {
+            int curr_cols_sum = 0;
+            for(int j = 0 ; j < n ; j++)
+            {
+                curr_cols_sum += matrix[j][i];
+            }
+            int diff = maxsum - curr_cols_sum;
+            operations += diff;
+        }
+    } 
 };
 
 //{ Driver Code Starts.
 int main()
 {
     int t;
-    cin >> t;
-    while (t--)
+    cin>>t;
+    while(t--)
     {
         int n;
-        cin >> n;
-        vector<vector<int> > matrix(n, vector<int>(n));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                cin >> matrix[i][j];
+        cin>>n;
+        vector<vector<int> > matrix (n,vector<int>(n));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                cin>>matrix[i][j];
         Solution ob;
         cout << ob.findMinOpeartion(matrix, n) << endl;
     }
     return 0;
 }
+
 
 // } Driver Code Ends
