7fe49ba94fb39d94027424d2a33329588cc133ba
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-06 11:05:42 +0530

    OOPS was necessary

--- hashingintro.cpp
+++ hashingintro.cpp
@@ -1,2 +1,50 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <list>
+using namespace std;
+
+struct Myhash
+{
+    int bucket;
+    list<int> *table;
+    Myhash(int b)
+    {
+        bucket = b;
+        table = new list<int>[b];
+    }
+
+    void insert(int key)
+    {
+        int i = key % bucket;
+        table[i].push_back(key);
+        cout << key << " now added." << endl;
+    }
+    bool search(int key)
+    {
+        int i = key % bucket;
+        for (int x : table[i])
+        {
+            if (x == key)
+            {   
+                cout << key << " is present." << endl;
+                return true;
+            }
+        }
+        cout << key << " is not present." << endl;
+        return false;
+    }
+    void remove(int key)
+    {
+        int i = key % bucket;
+        table[i].remove(key);
+        cout << key << " now removed." << endl;
+    }
+};
+
+int main()
+{
+    Myhash hash1 = Myhash(7);
+    hash1.insert(23);
+    hash1.search(23);
+    hash1.remove(23);
+    hash1.search(23);
+}
