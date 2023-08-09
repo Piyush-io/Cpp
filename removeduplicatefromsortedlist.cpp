5eaf72f250b2ee8d6a309522f70f44c8c8bc6671
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-09 20:57:48 +0530

    Your commit message

--- removeduplicatefromsortedlist.cpp
+++ removeduplicatefromsortedlist.cpp
@@ -2,58 +2,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node
+{
   int data;
   struct Node *next;
-  Node(int x) {
+  Node(int x)
+  {
     data = x;
     next = NULL;
   }
 };
 
-
 void print(Node *root)
 {
-    Node *temp = root;
-    while(temp!=NULL)
-    {
-    cout<<temp->data<<" ";
-    temp=temp->next;
-    }
+  Node *temp = root;
+  while (temp != NULL)
+  {
+    cout << temp->data << " ";
+    temp = temp->next;
+  }
 }
-Node* removeDuplicates(Node *root);
-int main() {
-	// your code goes here
-	int T;
-	cin>>T;
+Node *removeDuplicates(Node *root);
+int main()
+{
+  // your code goes here
+  int T;
+  cin >> T;
 
-	while(T--)
-	{
-		int K;
-		cin>>K;
-		Node *head = NULL;
-        Node *temp = head;
+  while (T--)
+  {
+    int K;
+    cin >> K;
+    Node *head = NULL;
+    Node *temp = head;
 
-		for(int i=0;i<K;i++){
-		int data;
-		cin>>data;
-			if(head==NULL)
-			head=temp=new Node(data);
-			else
-			{
-				temp->next = new Node(data);
-				temp=temp->next;
-			}
-		}
-		
-		Node *result  = removeDuplicates(head);
-		print(result);
-		cout<<endl;
-	}
-	return 0;
+    for (int i = 0; i < K; i++)
+    {
+      int data;
+      cin >> data;
+      if (head == NULL)
+        head = temp = new Node(data);
+      else
+      {
+        temp->next = new Node(data);
+        temp = temp->next;
+      }
+    }
+
+    Node *result = removeDuplicates(head);
+    print(result);
+    cout << endl;
+  }
+  return 0;
 }
 // } Driver Code Ends
-
 
 /*
 struct Node {
@@ -65,26 +67,26 @@ struct Node {
   }
 };*/
 
-//Function to remove duplicates from sorted linked list.
+// Function to remove duplicates from sorted linked list.
 Node *removeDuplicates(Node *head)
 {
-    if (head == NULL || head->next == NULL)
-    {
-        return head;
-    }
-
-    Node *curr = head;
-    Node *temp;
-    while (curr != NULL && curr->next != NULL)
-    {
-        if (curr->data == curr->next->data)
-        {
-            Node *temp = curr->next;
-            curr->next = temp->next;
-            delete temp;
-            continue;
-        }
-        curr = curr->next;
-    }
+  if (head == NULL || head->next == NULL)
+  {
     return head;
+  }
+
+  Node *curr = head;
+  Node *temp;
+  while (curr != NULL && curr->next != NULL)
+  {
+    if (curr->data == curr->next->data)
+    {
+      Node *temp = curr->next;
+      curr->next = temp->next;
+      delete temp;
+      continue;
+    }
+    curr = curr->next;
+  }
+  return head;
 }
