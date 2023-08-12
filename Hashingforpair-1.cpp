72e6b9382daa38495a71cecf7bdbcab0938ba965
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-12 13:03:02 +0530

    fun

--- Hashingforpair-1.cpp
+++ Hashingforpair-1.cpp
@@ -2,6 +2,8 @@
 // Initial Template for C++
 
 #include <bits/stdc++.h>
+#include <iostream>
+#include <set>
 using namespace std;
 
 int sumExists(int arr[], int N, int sum);
@@ -11,23 +13,45 @@ int sumExists(int arr[], int N, int sum);
 
 // You need to complete this function.
 // Function to check if there is a pair with the given sum in the array.
-int sumExists(int arr[], int N, int sum) {
-    // Your code here.
+int sumExists(int arr[], int N, int sum)
+{
+    set<int> s;
+    for (int i = 0; i < N; i++)
+    {
+        s.insert(arr[i]);
+    }
+
+    for (auto i = s.begin(); i != s.end(); i++)
+    {
+        int x = *i;
+        if (s.find(sum - x) != s.end())
+        {
+            if (s.find(sum - x) == x)
+            {
+                continue;
+            }
+            return true;
+        }
+    }
+    return false;
 }
 
 //{ Driver Code Starts.
 
-int main() {
+int main()
+{
 
     int testcases;
     cin >> testcases;
-    while (testcases--) {
+    while (testcases--)
+    {
         int N;
         cin >> N;
 
         int arr[N];
 
-        for (int i = 0; i < N; i++) cin >> arr[i];
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
         int sum;
         cin >> sum;
 
