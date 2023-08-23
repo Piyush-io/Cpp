c3fd8262cbdd644a7e0e5ace26cfc6ea319248d6
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-23 15:24:48 +0530

    ``` The diff is in the `checkifstirngsarerotationofeachother.cpp` file.
    The original code checks if two strings are rotations of each other by reversing the second string and checking if it is equal to the first string.
    
    The new code checks if the length of the two strings is equal. If not, it returns false. Otherwise, it concatenates the first string with itself and checks if the second string is a substring of the concatenated string.
    ``` (generated by useblackbox.ai )

--- checkifstirngsarerotationofeachother.cpp
+++ checkifstirngsarerotationofeachother.cpp
@@ -11,9 +11,11 @@ public:
     // Function to check if two strings are rotations of each other or not.
     bool areRotations(string s1, string s2)
     {
-        string rev = s2;
-        reverse(rev.begin(), rev.end());
-        return rev == s1;
+        if (s1.length() != s2.length())
+            return false;
+
+        string concatenated = s1 + s1;  // Concatenate the first string with itself
+        return concatenated.find(s2) != string::npos;
     }
 };
 
