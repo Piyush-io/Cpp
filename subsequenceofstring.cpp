3ec2fc5f7042748b22abb93a58440658941db5d3
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-24 12:57:10 +0530

    done

--- subsequenceofstring.cpp
+++ subsequenceofstring.cpp
@@ -13,9 +13,19 @@ class Solution
 public:
     // Function to check if a string is subsequence of other string.
 
-    bool recursiveisSubSequence(string A, string B, int n, int m)
+    bool recursiveisSubSequence(string s1, string s2, int n, int m)
     {
-        
+        if (m == 0)
+            return true;
+
+        if (n == 0)
+            return false;
+
+        if (s1[n - 1] == s2[m - 1])
+            return isSubSeq(s1, s2, n - 1, m - 1);
+
+        else
+            return isSubSeq(s1, s2, n - 1, m);
     }
 
     bool isSubSequence(string A, string B)
