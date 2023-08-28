7e175bc4a9ff968f76625d8c030035eb50f5ed5d
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-08-28 19:41:18 +0530

    damn that's sexy

--- minimumindex.cpp
+++ minimumindex.cpp
@@ -1,7 +1,60 @@
+//{ Driver Code Starts
+// Initial template for C++
+
 #include <iostream>
+#include <unordered_map>
 using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    // Function to find the minimum indexed character.
+    int minIndexChar(string str, string patt)
+    {
+        unordered_map<char, int> ele_present;
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (ele_present.find(str[i]) == ele_present.end())
+            {
+                ele_present[str[i]] = i;
+            }
+        }
+
+        int res = str.length();
+        for (int i = 0; i < patt.length(); i++)
+        {
+            if (ele_present.find(patt[i]) != ele_present.end())
+            {
+                res = min(res, ele_present[patt[i]]);
+            }
+        }
+        if (res == str.length())
+        {
+            return -1; // No match found
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
 
 int main()
 {
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        string patt;
+        cin >> str;
+        cin >> patt;
+        Solution obj;
+        cout << obj.minIndexChar(str, patt) << endl;
+    }
+    return 0;
 }
+
+// } Driver Code Ends
---
+++ modifiedstrings.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+#define br char xx; cin>>xx; cout<<xx<<endl;
+#define lli long long int
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find minimum number of characters which Ishaan must insert  
+    //such that string doesn't have three consecutive same characters.
+    int modified (string a)
+    {
+        int count = 0;
+        int res = 0;
+        for(int i = 0 ; i < a.length() ; i++)
+        {
+            
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    string a;
+    while(t--){
+        cin>>a;
+        Solution obj;
+        cout<<obj.modified(a)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
