8dbee28cff0fd973380a398c9a0a7e7d788c4e0a
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-04 21:34:52 +0530

    bruhhhhhh

--- linkedlist1.cpp
+++ linkedlist1.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-struct node
+struct node // structure of name node (not using class becasue of longer code required)
 {
-    int datapoint;
-    node *next;
-    node(int x)
+    int datapoint;  // the part of memory that will store the value of that node
+    node *next;  // part of memory that will store the address to next structure type node
+    node(int x)  // constructor just for the code to be pretty and to make our code shorter
     {
-        datapoint = x;
+        datapoint = x;   
         next = NULL;
     }
 };
 
-void printLinkedList(node *head)
+void printLinkedList(node *head)  // function to print the linked list
 {
     node *current = head;
     while (current != NULL)
@@ -25,8 +25,8 @@ void printLinkedList(node *head)
 
 int main()
 {
-    node * head = new node(10);
-    head->next = new node(20);
+    node * head = new node(10);  // pointer of type struct node pointing to a datapoint
+    head->next = new node(20);  // giving address to head node of next node
     head->next->next = new node(30);
     printLinkedList(head);
 }
