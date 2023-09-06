2151a958fcd7ee3e12816a3b6c9216dfc43554e2
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-06 22:58:17 +0530

    easy peasy

--- linkedlist1.cpp
+++ linkedlist1.cpp
@@ -3,29 +3,38 @@ using namespace std;
 
 struct node // structure of name node (not using class becasue of longer code required)
 {
-    int datapoint;  // the part of memory that will store the value of that node
-    node *next;  // part of memory that will store the address to next structure type node
-    node(int x)  // constructor just for the code to be pretty and to make our code shorter
+    int datapoint; // the part of memory that will store the value of that node
+    node *next;    // part of memory that will store the address to next structure type node
+    node(int x)    // constructor just for the code to be pretty and to make our code shorter
     {
-        datapoint = x;   
+        datapoint = x;
         next = NULL;
     }
 };
 
-void printLinkedList(node *node)  // function to print the linked list
+void printLinkedList(node *node) // function to print the linked list
 {
-    while (node != NULL) 
-    { 
-        cout<<node->datapoint<<" "; 
-        node = node->next; 
-    } 
+
+    if (node->next == NULL)
+    {
+        return;
+    }
+
+    while (node != NULL)
+    {
+        cout << node->datapoint << " ";
+        node = node->next;
+    }
 }
 
 int main()
 {
-    node * head = new node(10);  // head pointer to point to dynamically allocated memory of type "node" that is a structure
-    //we node is also a constructor, so when writing new node(10) it create a structure with datapoint 10 and next pointer set to null 
-    head->next = new node(20);  // giving address to head node of next node
+    node *head = new node(10); // head pointer to point to dynamically allocated memory of type "node" that is a structure
+    // we node is also a constructor, so when writing new node(10) it create a structure with datapoint 10 and next pointer set to null
+    head->next = new node(20); // giving address to head node of next node
     head->next->next = new node(30);
     printLinkedList(head);
+
+    delete head;
+    head = nullptr;
 }
