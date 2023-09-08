42b4f7bef44ad9d5497781b618f6e638e2f9083e
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-08 11:25:58 +0530

    niceee

---
+++ Insertelementatbeginning.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int datapoint; // the part of memory that will store the value of that node
+    node *next;    // part of memory that will store the address to next structure type node
+    node(int x)    // constructor just for the code to be pretty and to make our code shorter
+    {
+        datapoint = x;
+        next = NULL;
+    }
+};
+
+void printLinkedList(node *node) // function to print the linked list
+{
+
+    if (node->next == NULL)
+    {
+        return;
+    }
+
+    while (node != NULL)
+    {
+        cout << node->datapoint << " ";
+        node = node->next;
+    }
+}
+
+void inserteleatbeginning(node *&head, int x)
+{
+    node *newhead = new node(x); // new node which will new head created with value x
+    newhead->next = head;        // newhead now points to the initial head node
+    head = newhead;             // assigning value of newhead to head pointer making it the new head
+}
+
+int main()
+{
+    node *head = new node(10); // head pointer to point to dynamically allocated memory of type "node" that is a structure
+    // we node is also a constructor, so when writing new node(10) it create a structure with datapoint 10 and next pointer set to null
+    head->next = new node(20); // giving address to head node of next node
+    head->next->next = new node(30);
+    printLinkedList(head);
+    int x;
+    cout << "Enter value you want to add: " << endl;
+    cin >> x;
+    inserteleatbeginning(head, x);
+    printLinkedList(head);
+    delete head;
+    head = nullptr;
+}
---
+++ insertatbeginngandendoflist.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+  }  
+  cout<<"\n";
+} 
+
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *&head, int x) 
+    {
+        Node *newhead = new Node(x); // new node which will new head created with value x
+        newhead->next = head;        // newhead now points to the initial head node
+        head = newhead;             // assigning value of newhead to head pointer making it the new head
+        return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       // Your code here
+       
+       Node *end = new Node(x);
+       
+       if (head == NULL) 
+       {
+        // If the list is empty, the new node becomes the head
+        return end;
+        }
+       Node * it = head;
+       while(it->next != NULL)
+       {
+           it = it->next;
+       }
+       
+       it->next = end;
+       end->next = NULL;
+       return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        struct Node *head = NULL;
+        for (int i = 0; i < n; ++i)
+        {
+            int data, indicator;
+            cin>>data;
+            cin>>indicator;
+            Solution obj;
+            if(indicator)
+                head = obj.insertAtEnd(head, data); 
+            else
+                head = obj.insertAtBegining(head, data);
+        }
+        printList(head); 
+    }
+    return 0; 
+} 
+
+
+// } Driver Code Ends
