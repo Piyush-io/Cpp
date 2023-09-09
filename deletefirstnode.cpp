2d832e4638c84d4f742f5c6469ab929178a05c57
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-09 16:59:45 +0530

    again easy

---
+++ deletefirstnode.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int datapoint; // the part of memory that will store the value of that node
+    node *next;    // part of memory that will store the address to next structure type node
+    node(int x)    // constructor just for the code to be pretty and to make our code shorter
+    {
+        datapoint = x;
+        next = NULL;
+    }
+};
+
+void printLinkedList(node *node) // function to print the linked list
+{
+
+    if (node->next == NULL)
+    {
+        return;
+    }
+
+    while (node != NULL)
+    {
+        cout << node->datapoint << " ";
+        node = node->next;
+    }
+
+    cout << endl;
+}
+
+node *deletefirstnode(node *&head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        node *newhead = head->next;
+        delete head;
+        return newhead;
+    }
+}
+
+int main()
+{
+    node *head = new node(10); // head pointer to point to dynamically allocated memory of type "node" that is a structure
+    // we node is also a constructor, so when writing new node(10) it create a structure with datapoint 10 and next pointer set to null
+    head->next = new node(20); // giving address to head node of next node
+    head->next->next = new node(30);
+    printLinkedList(head);
+    node *newhead = deletefirstnode(head);
+    printLinkedList(newhead);
+}
