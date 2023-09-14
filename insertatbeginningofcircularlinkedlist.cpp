325e9e4d12deb2bca111be086b85e9be8c002d01
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-14 18:38:17 +0530

    naive

---
+++ insertatbeginningofcircularlinkedlist.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+Node *createNode(int value)
+{
+    Node *newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+// this function takes O(N) time to finish inserting at the beginning
+Node *insertatbeginning(Node *head, int value)
+{
+    Node *newNode = createNode(value);
+
+    if (head == nullptr)
+    {
+        newNode->next = newNode; // Point to itself for circular behavior
+        return newNode;
+    }
+    else
+    {
+        newNode->next = head;
+        Node *curr = head;
+        while (curr->next != head)
+        {
+            curr = curr->next;
+        }
+        curr->next = newNode;
+        return newNode;
+    }
+}
+
+void display(Node *head)
+{
+    if (head == nullptr)
+    {
+        std::cout << "Circular Linked List is empty." << std::endl;
+        return;
+    }
+
+    Node *current = head;
+    do
+    {
+        std::cout << current->data << " ";
+        current = current->next;
+    } while (current != head);
+    std::cout << std::endl;
+}
+
+int main()
+{
+    Node *head = nullptr;
+
+    // Insert three elements
+    head = insertatbeginning(head, 10);
+    head = insertatbeginning(head, 20);
+    head = insertatbeginning(head, 30);
+    // Display the circular list
+    display(head);
+
+    return 0;
+}
