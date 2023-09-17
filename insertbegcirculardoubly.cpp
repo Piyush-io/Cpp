c68e23d48db782d5f70771b66a85d97f4ddddfa6
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-17 17:03:07 +0530

    uhhh

---
+++ insertbegcirculardoubly.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+    Node* prev;
+    
+    Node(int value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+class CircularDoublyLinkedList {
+private:
+    Node* head;
+
+public:
+    CircularDoublyLinkedList() : head(nullptr) {}
+
+    // Function to insert a node at the beginning of the list
+    void insertAtBeginning(int value) {
+        Node* newNode = new Node(value);
+        if (!head) {
+            head = newNode;
+            head->next = head;
+            head->prev = head;
+        } else {
+            newNode->next = head;
+            newNode->prev = head->prev;
+            head->prev->next = newNode;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Function to display the circular doubly linked list
+    void display() {
+        if (!head) {
+            std::cout << "List is empty." << std::endl;
+            return;
+        }
+
+        Node* current = head;
+        do {
+            std::cout << current->data << " ";
+            current = current->next;
+        } while (current != head);
+
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    CircularDoublyLinkedList list;
+
+    // Insert elements at the beginning
+    list.insertAtBeginning(30);
+    list.insertAtBeginning(20);
+    list.insertAtBeginning(10);
+
+    // Display the circular doubly linked list
+    list.display();
+
+    return 0;
+}
--- swapkthelementsfromends.cpp
+++ swapkthelementsfromends.cpp
@@ -3,85 +3,93 @@
 
 using namespace std;
 
-struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
 };
 
+Node *swapkthnode(Node *head, int num, int K);
 
-
-Node *swapkthnode(Node* head, int num, int K);
-
-void addressstore(Node **arr, Node* head)
+void addressstore(Node **arr, Node *head)
 {
-    Node* temp = head;
+    Node *temp = head;
     int i = 0;
-    while(temp){
+    while (temp)
+    {
         arr[i] = temp;
         i++;
         temp = temp->next;
     }
 }
 
-bool check(Node ** before, Node **after, int num, int K)
+bool check(Node **before, Node **after, int num, int K)
 {
-    if(K > num)
+    if (K > num)
         return 1;
-        
-    bool f=true;
-    
-    for(int i=0; i<num; i++){
-        if((i==K-1) || (i==num - K)){
-            if(!((before[K-1] == after[num - K]) && (before[num-K] == after[K-1]))) f=0;
+
+    bool f = true;
+
+    for (int i = 0; i < num; i++)
+    {
+        if ((i == K - 1) || (i == num - K))
+        {
+            if (!((before[K - 1] == after[num - K]) && (before[num - K] == after[K - 1])))
+                f = 0;
         }
-        else{
-            if(before[i] != after[i]) f=0;
+        else
+        {
+            if (before[i] != after[i])
+                f = 0;
         }
     }
-    
+
     return f;
 }
 
 int main()
 {
     int T;
-    cin>>T;
-    while(T--){
-        int num, K , firstdata;
-        cin>>num>>K;
+    cin >> T;
+    while (T--)
+    {
+        int num, K, firstdata;
+        cin >> num >> K;
         int temp;
-        cin>>firstdata;
-        Node* head = new Node(firstdata);
-        Node* tail = head;
-        for(int i = 0; i<num - 1; i++){
-            cin>>temp;
+        cin >> firstdata;
+        Node *head = new Node(firstdata);
+        Node *tail = head;
+        for (int i = 0; i < num - 1; i++)
+        {
+            cin >> temp;
             tail->next = new Node(temp);
             tail = tail->next;
         }
-        
+
         Node *before[num];
         addressstore(before, head);
-        
+
         head = swapkthnode(head, num, K);
-        
+
         Node *after[num];
         addressstore(after, head);
-        
-        if(check(before, after, num, K))
-            cout<<1<<endl;
+
+        if (check(before, after, num, K))
+            cout << 1 << endl;
         else
-            cout<<0<<endl;
+            cout << 0 << endl;
     }
 }
 
 // } Driver Code Ends
 
 #include <vector>
-//User function Template for C++
+// User function Template for C++
 
 /* Linked List Node structure
    struct Node  {
@@ -94,7 +102,9 @@ int main()
   }
 */
 
-//Function to swap Kth node from beginning and end in a linked list.
+// Function to swap Kth node from beginning and end in a linked list.
+
+/*
 Node *swapkthnode(Node* head, int num, int k)
 {
     Node *temp = head;
@@ -123,4 +133,60 @@ Node *swapkthnode(Node* head, int num, int k)
         temp->next = NULL;
         return head;
     }
+}
+*/
+
+Node *swapkthnode(Node *head, int num, int k)
+{
+    if (num < K)
+        return head;
+    int cnt1 = K, cnt2 = num - K + 1;
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *node1 = NULL;
+    Node *node2 = NULL;
+    Node *node1Next = NULL;
+    Node *node2Prev = NULL;
+    Node *node1Prev = NULL;
+    Node *node2Next = NULL;
+    while (curr && (cnt1 > 0 || cnt2 > 0))  // curr not becoming null and count of both are not 0
+    {
+        cnt1--;
+        cnt2--;
+        if (cnt1 == 0 && node1 == NULL)
+        {
+            if (prev)  // is not null?
+                node1Prev = prev;
+            node1 = curr;
+            if (curr->next) // not null right?
+                node1Next = curr->next;
+        }
+        if (cnt2 == 0 && node2 == NULL)
+        {
+            if (prev)
+                node2Prev = prev;
+            node2 = curr;
+            if (curr->next)
+                node2Next = curr->next;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+    if (node1Prev && node1Prev != node2)
+        node1Prev->next = node2;
+    if (node2Prev && node2Prev != node1)
+        node2Prev->next = node1;
+    if (node1 != node2Next)
+        node1->next = node2Next;
+    else
+        node1->next = node2;
+    if (node2 != node1Next)
+        node2->next = node1Next;
+    else
+        node2->next = node1;
+    if (node1 == head)
+        head = node2;
+    else if (node2 == head)
+        head = node1;
+    return head;
 }
