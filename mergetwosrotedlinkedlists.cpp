93cf5b6be5f1bd5e8e50c79fea5e4dc203199c5d
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-09-23 12:57:39 +0530

    damn

--- mergetwosrotedlinkedlists.cpp
+++ mergetwosrotedlinkedlists.cpp
@@ -1,5 +1,5 @@
 //{ Driver Code Starts
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 /* Link list Node */
@@ -7,7 +7,7 @@ struct Node
 {
     int data;
     struct Node *next;
-    
+
     Node(int x)
     {
         data = x;
@@ -15,12 +15,12 @@ struct Node
     }
 };
 
-Node* sortedMerge(struct Node* a, struct Node* b);
+Node *sortedMerge(struct Node *a, struct Node *b);
 
 /* Function to print Nodes in a given linked list */
 void printList(struct Node *n)
 {
-    while (n!=NULL)
+    while (n != NULL)
     {
         cout << n->data << " ";
         n = n->next;
@@ -32,29 +32,29 @@ void printList(struct Node *n)
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-        int n,m;
-        cin>>n>>m;
+        int n, m;
+        cin >> n >> m;
 
         int data;
-        cin>>data;
+        cin >> data;
         struct Node *head1 = new Node(data);
         struct Node *tail1 = head1;
         for (int i = 1; i < n; ++i)
         {
-            cin>>data;
+            cin >> data;
             tail1->next = new Node(data);
             tail1 = tail1->next;
         }
 
-        cin>>data;
+        cin >> data;
         struct Node *head2 = new Node(data);
         struct Node *tail2 = head2;
-        for(int i=1; i<m; i++)
+        for (int i = 1; i < m; i++)
         {
-            cin>>data;
+            cin >> data;
             tail2->next = new Node(data);
             tail2 = tail2->next;
         }
@@ -67,22 +67,57 @@ int main()
 
 // } Driver Code Ends
 
-
- 
-
 /* Link list Node
 struct Node {
   int data;
   struct Node *next;
-  
+
   Node(int x) {
     data = x;
     next = NULL;
   }
 };
 */
-//Function to merge two sorted linked list.
-Node* sortedMerge(Node* head1, Node* head2)  
-{  
-    // code here
-}  
+// Function to merge two sorted linked list.
+Node *sortedMerge(Node *a, Node *b)
+{
+    if (a == NULL)
+        return b;
+    if (b == NULL)
+        return a;
+    Node *head = NULL, *tail = NULL;
+    if (a->data <= b->data)
+    {
+        head = tail = a;
+        a = a->next;
+    }
+    else
+    {
+        head = tail = b;
+        b = b->next;
+    }
+    while (a != NULL && b != NULL)
+    {
+        if (a->data <= b->data)
+        {
+            tail->next = a;
+            tail = a;
+            a = a->next;
+        }
+        else
+        {
+            tail->next = b;
+            tail = b;
+            b = b->next;
+        }
+    }
+    if (a == NULL)
+    {
+        tail->next = b;
+    }
+    else
+    {
+        tail->next = a;
+    }
+    return head;
+}
