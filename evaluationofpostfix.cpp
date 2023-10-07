63f989a33b217bab29d606a7ddd091eaf001b4c2
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-10-07 17:40:26 +0530

    easy

---
+++ evaluationofpostfix.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// C++ program to evaluate value of a postfix expression
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+private:
+    bool isOperand(char c)
+    {
+        return (c >= '0' && c <= '9');
+    }
+
+    bool isOperator(char c)
+    {
+        return c == '*' || c == '/' || c == '+' || c == '-' || c == '^';
+    }
+
+public:
+    // Function to evaluate a postfix expression.
+    int evaluatePostfix(string S)
+    {
+        int res = 0;
+        stack<int> exp;
+        for (auto x : S)
+        {
+            if (isOperand(x))
+            {
+                exp.push(x - '0');
+            }
+
+            else if (isOperator(x))
+            {
+                int temp;
+                int op1 = exp.top();
+                exp.pop();
+                int op2 = exp.top();
+                exp.pop();
+                switch (x)
+                {
+                case '*':
+                    temp = op2 * op1;
+                    break;
+                case '/':
+                    temp = op2 / op1;
+                    break;
+                case '+':
+                    temp = op2 + op1;
+                    break;
+                case '-':
+                    temp = op2 - op1;
+                    break;
+                case '^':
+                    temp = pow(op2, op1);
+                }
+                exp.push(temp);
+            }
+        }
+        return exp.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(INT_MAX, '\n');
+    while (t--)
+    {
+        string S;
+        cin >> S;
+        Solution obj;
+
+        cout << obj.evaluatePostfix(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
