389408ea7e20518b0458a8debea7908b68fba5fa
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-10-13 19:36:04 +0530

    BT basic structure

---
+++ BT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node(int x)
+    {
+        key = x;
+        left = right = NULL;
+    }
+};
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+}
