eee2dcd32606c797946ae3a1affac1101698a709
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-10-15 16:15:40 +0530

    this would require some practice

--- BT.cpp
+++ BT.cpp
@@ -13,9 +13,22 @@ struct Node
     }
 };
 
+void displaytree(Node *ROOT)
+{
+    if (ROOT != NULL)
+    {
+        displaytree(ROOT->left);
+        cout << ROOT->key << " ";
+        displaytree(ROOT->right);
+    }
+}
+
 int main()
 {
     Node *root = new Node(10);
     root->left = new Node(20);
     root->right = new Node(30);
+    root->right->left = new Node(40);
+    root->right->right = new Node(50);
+    displaytree(root);
 }
