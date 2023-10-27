b11bf62c9b94da6aae8991c6691723909ae4ca82
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-10-27 11:22:15 +0530

    Inorder traversal of BST

--- insertinBST.cpp
+++ insertinBST.cpp
@@ -1,171 +1,79 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX_HEIGHT 100000
 
-// Tree Node
-struct Node
-{
+struct Node {
     int data;
     Node *left;
     Node *right;
-
-    Node(int val)
-    {
-        data = val;
-        left = right = NULL;
-    }
 };
 
-// } Driver Code Ends
-// Function to insert a node in a BST.
+Node *createNewNode(int value) {
+    Node *temp = new Node;
+    temp->data = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
 
-/*
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-*/
-
-class Solution
-{
-public:
-    Node *insert(Node *root, int data)
-    {
-        if (root == NULL)
-        {
-            return new Node(data);
-        }
-
-        else if (data < root->data)
-        {
-            root->left = insert(root->left, data);
-        }
-
-        else if (data > root->data)
-        {
-            root->right = insert(root->right, data);
-        }
-
-        else
-        {
-            return root;
-        }
-    }
-};
-
-//{ Driver Code Starts.
-
-// Function to Build Tree
-Node *buildTree(string str)
-{
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node *> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size())
-    {
-
-        // Get and remove the front of the queue
-        Node *currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N")
-        {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N")
-        {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
+Node *newNode(Node *root, int data) {
+    if (root == NULL)
+        root = createNewNode(data);
+    else if (data < root->data)
+        root->left = newNode(root->left, data);
+    else
+        root->right = newNode(root->right, data);
 
     return root;
 }
 
-void inorder(Node *root, vector<int> &v)
-{
-    if (root == NULL)
-        return;
 
-    inorder(root->left, v);
-    v.push_back(root->data);
-    inorder(root->right, v);
+// } Driver Code Ends
+// User function Template for C++
+
+// Function to return a list containing the inorder traversal of the BST.
+
+// helper function to pass vector by reference
+void ino(Node *root,vector<int> &ans)
+{
+    if(root!=NULL)
+    {
+        ino(root->left,ans);
+        ans.push_back(root->data);
+        ino(root->right,ans);
+    }
 }
 
-int main()
+vector<int> inOrder(Node *root) 
 {
+    vector<int> v;
+    ino(root,v);
+    return v;
+}
 
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--)
-    {
-        string s;
-        getline(cin, s);
-        Node *root = buildTree(s);
+//{ Driver Code Starts.
 
-        getline(cin, s);
-        int k = stoi(s);
-        // getline(cin, s);
-        Solution ob;
-        ob.insert(root, k);
-        vector<int> v;
-        inorder(root, v);
-        for (int i = 0; i < v.size(); i++)
-            cout << v[i] << " ";
+int main() {
+
+    int testcases;
+    cin >> testcases;
+    while (testcases--) {
+        Node *root = NULL;
+        int sizeOfArray;
+        cin >> sizeOfArray;
+        int arr[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) cin >> arr[i];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            root = newNode(root, arr[i]);
+        }
+
+        vector<int> res = inOrder(root);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
         cout << endl;
-
-        // cout<<"~"<<endl;
     }
     return 0;
 }
-
 // } Driver Code Ends
