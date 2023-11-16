70dfab20fe6f310fc375ac840d3e4b7826b9562d
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-11-16 20:25:18 +0530

    Time: 3 ms (74.62%) | Memory: 10.7 MB (52.42%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-
+/*
     void generateBinaryCombinations(int n, std::unordered_map<string, int> &binaryMap)  
     {
         int totalCombinations = 1 << n;
@@ -11,11 +11,11 @@ public:
             bitset<32> binary(i);
             binaryMap[binary.to_string().substr(32 - n)] = 0;
         }
-    }
+    } */
 
     string findDifferentBinaryString(vector<string>& nums) 
     {
-        if(nums.size() == 0) return nums[0] == "1" ? "0" : "1";
+        /* if(nums.size() == 0) return nums[0] == "1" ? "0" : "1";
         unordered_map<string,int> mp;
         generateBinaryCombinations(nums.size() , mp);
         for(int i = 0 ; i < nums.size() ; i++)
@@ -29,6 +29,20 @@ public:
                 return x.first;
         }
 
-        return "";
+        return ""; */
+
+        unordered_set<int> integers;
+        for (string num : nums) {
+            integers.insert(stoi(num, 0, 2));
+        }
+        
+        int ans = stoi(nums[0], 0, 2);
+        int n = nums.size();
+        
+        while (integers.find(ans) != integers.end()) {
+            ans = rand() % (int) pow(2, n);
+        }
+        
+        return bitset<16>(ans).to_string().substr(16 - n);
     }
 };
