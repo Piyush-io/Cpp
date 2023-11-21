748a4a1e75a93fb1260d7b1dfa3c47f544108ab6
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-11-21 07:51:26 +0530

    Time: 99 ms (54.64%) | Memory: 57 MB (83.57%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.cpp
@@ -1,4 +1,6 @@
-/*
+class Solution {
+public:
+
     int reverseNum(int num)
     {
         int reversed = 0;
@@ -9,7 +11,7 @@
         }
         return reversed;
     }
-
+    
     int countNicePairs(vector<int>& nums) {
         unordered_map<int, int> count;
         int res = 0;
@@ -28,35 +30,5 @@
             }
         }
         return res;
-    }
-    */
-
-class Solution {
-public:
-    int rev(int n){
-        int r = 0;
-        while(n>0){
-             r *=10;
-             r +=(n%10);
-             n/=10;
-        }
-        return r;
-    }
-    int countNicePairs(vector<int>& nums) {
-        int n = nums.size();
-        int count =0;
-        unordered_map<int,int>m;
-        for(int i=0;i<n;i++){
-            nums[i]-=rev(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            if(m.find(nums[i])!=m.end()){
-                count=count%1000000007;
-                count +=m[nums[i]];
-                m[nums[i]]++;
-            }
-            else  m[nums[i]]++;
-        }
-        return count%1000000007;
     }
 };
