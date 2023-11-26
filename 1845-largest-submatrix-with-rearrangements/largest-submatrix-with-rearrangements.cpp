736fc11a56aa34b8fbe7243da05014ff1b5e6134
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-11-26 11:51:32 +0530

    Time: 124 ms (98.30%) | Memory: 66.1 MB (96.02%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,3 +1,6 @@
+#pragma GCC optimize("Ofast","inline","fast-math","unroll-loops","no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native","f16c")
+static const auto fast = []() {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); return 0; } ();
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
@@ -12,9 +15,11 @@ public:
         }
 
         int res = INT_MIN;
-        for(int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++) 
+        {
             sort(matrix[i].begin(), matrix[i].end(), greater<>());
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++) 
+            {
                 int temp = matrix[i][j] * (j + 1);
                 res = max(res, temp);
             }
