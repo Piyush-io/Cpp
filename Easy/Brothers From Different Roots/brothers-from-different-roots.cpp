9e85171bb49ca92f98dea8331236ea677a56a8b6
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-12-03 07:26:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Brothers From Different Roots/brothers-from-different-roots.cpp
@@ -0,0 +1,140 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+public:
+
+    void insertBSTNodesIntoSet(Node* root, unordered_set<int>& nodeSet) 
+    {
+        if (root == nullptr) {
+            return;
+        }
+        nodeSet.insert(root->data);
+        insertBSTNodesIntoSet(root->left, nodeSet);
+        insertBSTNodesIntoSet(root->right, nodeSet);
+    }
+
+    
+    int countPairs(Node* root1, Node* root2, int x) {
+        unordered_set<int> bst1;
+        unordered_set<int> bst2;
+        insertBSTNodesIntoSet(root1, bst1);
+        insertBSTNodesIntoSet(root2, bst2);
+        int res = 0;
+        for (auto i : bst1) {
+            if (bst2.find(x - i) != bst2.end()) 
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string tree1, tree2;
+        getline(cin, tree1);
+        getline(cin, tree2);
+        Node* root1 = buildTree(tree1);
+        Node* root2 = buildTree(tree2);
+        int x;
+        cin >> x;
+        cin.ignore();
+        Solution ob;
+        cout << ob.countPairs(root1, root2, x) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
