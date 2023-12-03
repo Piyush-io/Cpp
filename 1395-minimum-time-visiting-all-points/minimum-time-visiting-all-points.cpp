56f1a90ca8bf504c9afe63e3c47de4b65aab218e
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-12-03 07:15:57 +0530

    Time: 3 ms (95.33%) | Memory: 10.4 MB (64.64%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) 
+    {
         int k=0;
         for(int i=1;i<points.size();i++)
-        k+=std::max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]));
+        k+= max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]));
         return k;
     }
 };
