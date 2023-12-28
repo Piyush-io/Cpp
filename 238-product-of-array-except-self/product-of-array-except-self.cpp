86874e95409665566747df18aa447cf2599121a0
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2023-12-28 22:20:40 +0530

    Time: 53 ms (6.23%) | Memory: 24.2 MB (99.78%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-vector<int> productExceptSelf(vector<int>& a)
-    {
-        int n = a.size();
-        vector<int>v(n,1);
-        for(int i = n-2; i >= 0; i--) {
-           v[i] = v[i+1]*a[i+1];
-        }
-        int temp = 1;
+vector<int> productExceptSelf(std::vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 1);
+        int prefixProduct = 1;
         for (int i = 0; i < n; i++) {
-            v[i] = v[i]*temp;
-            temp = temp*a[i];
+            result[i] *= prefixProduct;
+            prefixProduct *= nums[i];
         }
-        return v;
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        return result;
     }
 };
