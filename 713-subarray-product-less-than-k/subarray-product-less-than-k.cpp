b0926d756c81bf9db22de3b5006310e29be9e484
Piyush Mittal <131678293+Piyush-io@users.noreply.github.com>
2024-03-27 08:38:42 +0530

    Time: 47 ms (92.68%) | Memory: 63.7 MB (57.48%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k <= 1) return 0; // If k is less than or equal to 1, no subarray can have product less than k
-
+    int numSubarrayProductLessThanK(vector<int>& n, int k) {
+        if(k==0) return 0;
+        long long int rp = 1;
         int res = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int temp = 1; // Initialize temp to 1 for each new subarray
-            for (int j = i; j < nums.size(); j++) {
-                temp *= nums[j]; // Update the product by multiplying with the next element
-                if (temp < k)
-                    res++; // Increment count for each valid subarray
-                else
-                    break; // Exit the inner loop if the product becomes greater than or equal to k
+        int ri = 0;
+        for(int i=0;i<n.size();i++){
+            rp = rp * n[i];
+            while(rp >= k && i>=ri) {
+                rp = rp / (n[ri++]);
             }
+            res = res + (i-ri+1);
         }
         return res;
     }
